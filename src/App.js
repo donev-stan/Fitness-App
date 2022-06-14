1d5ce018b34a654220cf7637f9ae19898ab3d01a
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2022-06-14 09:23:51 +0300

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Footer from "./components/Footer";
+
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
 
 function App() {
-  return null;
+  return (
+    <Box width={"400px"} sx={{ width: { x1: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
 }
 
 export default App;
